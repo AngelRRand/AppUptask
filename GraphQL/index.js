863dd0276344cd2f880e05f723e820241acc5f55
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-28 12:26:58 -0300

    break

--- GraphQL/index.js
+++ GraphQL/index.js
@@ -4,6 +4,10 @@ const typeDefs = require('./db/schema')
 const resolvers = require('./db/resolvers')
 const conectarDB = require('./config/db')
 
+
+conectarDB();
+
+
 const server = new ApolloServer( { typeDefs, resolvers })
 
 server.listen().then( ({url}) =>{
