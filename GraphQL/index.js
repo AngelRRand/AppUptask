bba87c474bbb05016f2c8c56adadac2918bcb757
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-28 00:45:30 -0300

    obtenerCursos

--- GraphQL/index.js
+++ GraphQL/index.js
@@ -4,10 +4,35 @@ const {ApolloServer, gql }= require('apollo-server')
 const typeDefs = gql`
 
     type Query {
-
+        obtenerCursos
     } 
-    
+
 `;
+
+const resolvers = {
+    Query: {
+        obtenerCursos : () => cursos
+    }
+}
+
+const cursos = [
+    {
+      titulo: "Javascript Moderno Guía definitiva Construye +10 Proyectos",
+      tecnologia: "Javascript ES6",
+    },
+    {
+      titulo: "React - La Guía Completa: Hooks Context Redux MERN +15 Apps",
+      tecnologia: "React",
+    },
+    {
+      titulo: "Node js - Bootcamp Desarrollo Web inc. MVC y REST API",
+      tecnologia: "React",
+    },
+    {
+      titulo: "React js - ReactJS Avanzado - FullStack React GraphQL y Apollo",
+      tecnologia: "React",
+    },
+  ];
 
 const server = new ApolloServer()
 
