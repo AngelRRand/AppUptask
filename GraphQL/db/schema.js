feb99be54411a890ff0e437bfd562b4c60e522ef
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-28 11:39:10 -0300

    componsition

---
+++ GraphQL/db/schema.js
@@ -0,0 +1,22 @@
+const { gql }= require('apollo-server');
+
+
+const typeDefs = gql`
+
+    type Curso {
+        titulo: String
+        
+    }
+
+    type Tecnologia {
+      tecnologia: String
+    }
+
+    type Query {
+        obtenerCursos : [Curso]
+        obtenerTecnologias: [Tecnologia]
+    } 
+
+`;
+
+module.exports = typeDefs;
--- GraphQL/index.js
+++ GraphQL/index.js
@@ -1,23 +1,6 @@
-const {ApolloServer, gql }= require('apollo-server')
+const {ApolloServer }= require('apollo-server')
 
 
-const typeDefs = gql`
-
-    type Curso {
-        titulo: String
-        
-    }
-
-    type Tecnologia {
-      tecnologia: String
-    }
-
-    type Query {
-        obtenerCursos : [Curso]
-        obtenerTecnologias: [Tecnologia]
-    } 
-
-`;
 
 const resolvers = {
     Query: {
