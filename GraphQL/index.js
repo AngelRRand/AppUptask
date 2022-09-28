2b5f9b821b8f64d1676cdd1b530fc6e12f129ea7
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-28 11:41:56 -0300

    schema

--- GraphQL/index.js
+++ GraphQL/index.js
@@ -1,33 +1,8 @@
 const {ApolloServer }= require('apollo-server')
 
+const typeDefs = require('./db/schema')
+const resolvers = require('./db/resolvers')
 
-
-const resolvers = {
-    Query: {
-        obtenerCursos : () => cursos,
-
-        obtenerTecnologias: () => tecnologia
-    }
-}
-
-const cursos = [
-    {
-      titulo: "Javascript Moderno Guía definitiva Construye +10 Proyectos",
-      tecnologia: "Javascript ES6",
-    },
-    {
-      titulo: "React - La Guía Completa: Hooks Context Redux MERN +15 Apps",
-      tecnologia: "React",
-    },
-    {
-      titulo: "Node js - Bootcamp Desarrollo Web inc. MVC y REST API",
-      tecnologia: "React",
-    },
-    {
-      titulo: "React js - ReactJS Avanzado - FulllStack React GraphQL y Apollo",
-      tecnologia: "React",
-    },
-  ];
 
 const server = new ApolloServer( { typeDefs, resolvers })
 
