b49d3c359569bc0ebfb7a6827cc666455cbcf05e
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-28 12:02:29 -0300

    conect

---
+++ GraphQL/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+require('dotenv').config({path: '.env'})
+
+
+const conectarDB = async () =>{
+    try {
+        
+    } catch (error) {
+        console.log('Hubo un error')
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = conectarDB;
