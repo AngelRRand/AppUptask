c23d4a43eb6e552cce22a5f15d89b70a75dbfe80
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-10 18:47:28 -0300

    install navigation

---
+++ Client/src/view/loginUser.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const loginUser = () => {
+  return (
+    <View>
+      <Text>loginUser</Text>
+    </View>
+  )
+}
+
+export default loginUser
