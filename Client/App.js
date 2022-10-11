0cf817c5dbfa338943380801251c34ec714156d0
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-11 02:31:32 -0300

    finish App

--- Client/App.js
+++ Client/App.js
@@ -1,37 +1,99 @@
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import LoginUser from './src/view/LoginUser';
-import CreateUser from './src/view/CreateUser';
-import Home from './src/view/Home';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Root } from 'native-base'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
+import Login from './views/Login';
+import CrearCuenta from './views/CrearCuenta';
+import Proyectos from './views/Proyectos';
+import NuevoProyecto from './views/NuevoProyecto';
+import Proyecto from './views/Proyecto';
 
+import { ApolloProvider } from '@apollo/client'
+import client from './config/apollo';
 
-const Stack = createNativeStackNavigator();
-
-export default function App() {
+const App = () => {
   return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='LoginUser'>
-          <Stack.Screen
-            name='LoginUser'
-            component={LoginUser}
-            options={{
-              title: 'Iniciar Sesion',
-              headerShown: false
-            }}
-          />
-          <Stack.Screen
-            name='CreateUser'
-            component={CreateUser}
-            options={{
-              title: 'Crear Cuenta',
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
-  );
-}
+    <ApolloProvider client={client}>
+      <Root>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login" >
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{
+                title: "Iniciar Sesión",
+                headerShown: false
+              }}
+            />
 
+            <Stack.Screen
+              name="CrearCuenta"
+              component={CrearCuenta}
+              options={{
+                title: "Crear Cuenta",
+                headerStyle: {
+                  backgroundColor: '#28303B'
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                  fontWeight: 'bold'
+                }
+              }}
+            />
+
+            <Stack.Screen
+              name="Proyectos"
+              component={Proyectos}
+              options={{
+                title: "Proyectos",
+                headerStyle: {
+                  backgroundColor: '#28303B'
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                  fontWeight: 'bold'
+                }
+              }}
+            />
+
+            <Stack.Screen
+              name="NuevoProyecto"
+              component={NuevoProyecto}
+              options={{
+                title: "Nuevo Proyecto",
+                headerStyle: {
+                  backgroundColor: '#28303B'
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                  fontWeight: 'bold'
+                }
+              }}
+            />
+
+            <Stack.Screen
+              name="Proyecto"
+              component={Proyecto}
+              options={({ route }) => ({
+                title: route.params.nombre,
+                headerStyle: {
+                  backgroundColor: '#28303B'
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                  fontWeight: 'bold'
+                }
+              })}
+            />
+
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Root>
+    </ApolloProvider>
+  );
+};
+
+export default App;
